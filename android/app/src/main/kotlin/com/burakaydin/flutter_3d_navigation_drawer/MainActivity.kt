da68711542f90c4dd28a894196fd572c001b28d4
aydnburak <burakaydin9761@gmail.com>
2021-03-28 15:27:53 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/burakaydin/flutter_3d_navigation_drawer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.burakaydin.flutter_3d_navigation_drawer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blue.shade400,
+                  Colors.blue.shade800,
+                ],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Container(
+              width: 200,
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: <Widget>[
+                  DrawerHeader(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                              "https://img-s1.onedio.com/id-56e08f2537a2ba207ab3917c/rev-0/w-300/h-450/f-jpg/s-64660ce43f1c3226608370e986b3a794ad08446d.jpg"),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "John Smith",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView(
+                      children: <Widget>[
+                        ListTile(
+                          onTap: () {},
+                          leading: Icon(
+                            Icons.home,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            "Home",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        ListTile(
+                          onTap: () {},
+                          leading: Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            "Profile",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        ListTile(
+                          onTap: () {},
+                          leading: Icon(
+                            Icons.settings,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            "Settings",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        ListTile(
+                          onTap: () {},
+                          leading: Icon(
+                            Icons.logout,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            "Log out",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          TweenAnimationBuilder(
+              tween: Tween<double>(begin: 0, end: value),
+              duration: Duration(milliseconds: 500),
+              builder: (_, double val, __) {
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.001)
+                    ..setEntry(0, 3, 200 * val)
+                    ..rotateY((pi / 6) * val),
+                  child: Scaffold(
+                    appBar: AppBar(
+                      title: Text("3D Drawer Menu"),
+                    ),
+                    body: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("Menüyü Açmak İçin Sağa Kaydırın 👉 "),
+                          ElevatedButton(onPressed: () {}, child: Text("Bas"))
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }),
+          GestureDetector(
+            onHorizontalDragUpdate: (e) {
+              if (e.delta.dx > 0) {
+                setState(() {
+                  value = 1;
+                });
+              } else {
+                setState(() {
+                  value = 0;
+                });
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
